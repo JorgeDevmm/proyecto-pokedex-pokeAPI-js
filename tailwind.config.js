f4d26c14e1e96acb365c6cdac2a72a7866b17ce3
JorgeDevmm <jorge29luis10@gmail.com>
2024-02-12 23:44:35 -0500

    Estructurando proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // ruta de para que pueda leer archivo js y html dentro de sr dentro o fuera de subcarpetas, tambien "./index.html"
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
